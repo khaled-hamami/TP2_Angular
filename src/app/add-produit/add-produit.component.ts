1874ded9f1eef9e41d3a1bfe96122d5beb7633c3
khaled hammami <khaled.hammami740@gmail.com>
2023-09-28 15:46:19 +0100

    angular project -tp1

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+  styleUrls: ['./add-produit.component.css']
+})
+export class AddProduitComponent {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'produits', component: ProduitsComponent },
+  { path: 'add-produit', component: AddProduitComponent },
+  { path: '', redirectTo: 'produits', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
